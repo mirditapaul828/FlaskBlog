9a47b9a41fde1980382ec5a4e443a14744590555
mirditapaul828 <mirditapaul828@gmail.com>
2021-02-09 15:56:17 -0500

    Added Email and Password Reset Functionality

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask  #Import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)       #Set app to an instance of flask
 app.config['SECRET_KEY'] = '10760aebb0732e9d9083454f7604db15' #Create a secret key
@@ -11,5 +13,13 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+
+#Env variables stored locally
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,13 +1,15 @@
-#WT Forms is the most popular extension to work with forms
+# WT Forms is the most popular extension to work with forms
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField  #This enables us to put strings in the field
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, \
+    TextAreaField  # This enables us to put strings in the field
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
-#Create Registration Form
-#When creating a form, you have to create a class for it first
+
+# Create Registration Form
+# When creating a form, you have to create a class for it first
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -21,9 +23,10 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('That username is taken. Please choose a different username')
 
     def validate_email(self, email):
-        email = User.query.filter_by(email=email.data).first()
-        if email:
+        user = User.query.filter_by(email=email.data).first()
+        if user:
             raise ValidationError('Email has already been taken')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -31,7 +34,8 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me!')
     submit = SubmitField('Login')
 
-#Update Profile form
+
+# Update Profile form
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
@@ -55,3 +59,20 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Reset Password')
